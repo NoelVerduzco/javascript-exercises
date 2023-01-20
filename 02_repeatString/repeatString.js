cefaa328ad94fccf30a18c0af30e0e6a9522235b
noelverduzco <noeljverduzco02@gmail.com>
2023-01-19 22:01:40 -0800

    Complete 01_helloWorld and 02_repeatString JS exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let longString = "";
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            longString += string;
+        }
+    } else {
+        return "ERROR";
+    }
+    return longString;
 };
 
 // Do not edit below this line
