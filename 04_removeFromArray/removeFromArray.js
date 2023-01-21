11c05d7cf2330dfa9e0590d84b0bdb7ecdefa7a1
noelverduzco <noeljverduzco02@gmail.com>
2023-01-21 14:46:50 -0800

    Complete 04_removeFromArray Javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...removeItem) {
+    let testArray = array;
 
+    for (let i = 0; i < removeItem.length; i++) {
+	    for (let item of testArray) {
+	        if (item === removeItem[i]) {
+	            testArray.splice(testArray.indexOf(item),1);
+	        }
+	    }
+    }
+
+    return testArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// Look through every item in the array
+// If the item in the array matches the items that need removing, remove it
+// Return the final item
