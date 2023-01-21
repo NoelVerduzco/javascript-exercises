f8af373479397a40a00dd39c251a81f73a3dd90e
noelverduzco <noeljverduzco02@gmail.com>
2023-01-21 13:38:21 -0800

    Complete 03_reverseString JavaScript exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringToList = string.split("");
+    reverseTheList = stringToList.reverse();
+    listToString = reverseTheList.toString();
+    removeCommas = listToString.replaceAll(",","");
+    return removeCommas;
 };
 
 // Do not edit below this line
