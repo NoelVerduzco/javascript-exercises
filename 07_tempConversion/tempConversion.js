5972d4fb228921b739d45010311f20ce25d58fb6
noelverduzco <noeljverduzco02@gmail.com>
2023-01-22 12:01:25 -0800

    Complete 07_tempConversion JavaScript exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let fTemp = temp;
+  let cTemp = (fTemp - 32) * (5/9);
+
+  return Number(cTemp.toPrecision(3));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let cTemp = temp;
+  let fTemp = (cTemp * 9 / 5) + 32;
+
+  return Number(fTemp.toPrecision(4));
 };
 
 // Do not edit below this line
