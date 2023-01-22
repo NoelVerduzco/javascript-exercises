a67c659b167c692badb9ecdd884eb3e9a01a89a3
noelverduzco <noeljverduzco02@gmail.com>
2023-01-21 16:27:15 -0800

    Complete 05_sumAll JavaScript exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(leftNum,rightNum) {
+    left = leftNum;
+    right = rightNum;
+    sum = 0;
 
+    if (left < 0 || right < 0) return "ERROR";
+    if (typeof left === "string" || typeof right === "string") return "ERROR";
+    if (isNaN(left) || isNaN(right)) return "ERROR";
+    if (left > right) {
+        let temp = 0;
+        temp = right;
+        right = left;
+        left = temp;
+    }
+
+    for (let  i = left; i < right + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
