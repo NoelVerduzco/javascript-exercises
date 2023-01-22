38fad069b6abfa622b3e458a931552f0fdaf3822
noelverduzco <noeljverduzco02@gmail.com>
2023-01-22 11:38:20 -0800

    Complete 06_leapYears JavaScript exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    thisYear = year;
 
-};
-
+    if (thisYear % 4 == 0) {
+        if (thisYear % 100 == 0 && thisYear % 400 === 0) return true;
+        if (thisYear % 100 !== 0) {
+            return true
+        } return false;
+    } else return false;
+}
 // Do not edit below this line
 module.exports = leapYears;
