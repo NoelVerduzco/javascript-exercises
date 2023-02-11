15933a41b8ad2cc957ab8c0290bacd0f68ad10db
noelverduzco <noeljverduzco02@gmail.com>
2023-02-10 20:28:11 -0800

    Complete 09_palindromes JS exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+    let chars = str.split("");
+    let letters = [];
+    for (let char of chars) {
+        if (char.toUpperCase() != char.toLowerCase()) {
+            letters.push(char);
+        }
+    }
+    if (letters.toString().toLowerCase() === letters.reverse().toString().toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// intake string
+// convert to array
+// loop through array and check if character is letter
+// if true, keep and add into new array
+// check if new array === new array reversed
+// if so, return true
