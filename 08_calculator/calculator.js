4fed93d6da245e966381dfe4604bcaaf7f320f3e
noelverduzco <noeljverduzco02@gmail.com>
2023-02-10 19:00:16 -0800

    Complete 08_calculator JS exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((total, num) => {
+    return total + num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, num) => {
+    return total * num;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(N) {
+	if (N === 0) return 1;
+  let total = 1;
+  for (let i = 1; i <= N; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
