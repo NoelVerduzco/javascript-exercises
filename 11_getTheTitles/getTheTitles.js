10cf8538620dcaa05c436e2a9f9edafcceb1c1cf
noelverduzco <noeljverduzco02@gmail.com>
2023-02-11 16:22:37 -0800

    Complete 11_getTheTitles JS exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const bookTitles = [];
 
+    books.map(book => {
+        bookTitles.push(book.title);
+    });
+    
+    return bookTitles;
 };
 
 // Do not edit below this line
